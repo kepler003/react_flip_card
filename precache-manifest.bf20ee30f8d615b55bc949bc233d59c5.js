7f6de1d961209cfcdd2e5e42b8523e52ed6c0368
Bartłomiej Pyzocha <b.pyzocha@gmail.com>
2020-05-24 16:59:32 +0200

    Updates

---
+++ precache-manifest.bf20ee30f8d615b55bc949bc233d59c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd81ba4271e8933bc5efe41dd8dcb758",
+    "url": "/react_flip_card/index.html"
+  },
+  {
+    "revision": "62b71151306d2d3fb16a",
+    "url": "/react_flip_card/static/css/main.e8b6b874.chunk.css"
+  },
+  {
+    "revision": "fafebb1cb8a0bc1ea7f1",
+    "url": "/react_flip_card/static/js/2.9656a956.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_flip_card/static/js/2.9656a956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b71151306d2d3fb16a",
+    "url": "/react_flip_card/static/js/main.8dd476ba.chunk.js"
+  },
+  {
+    "revision": "768e672ad789fc00c77c",
+    "url": "/react_flip_card/static/js/runtime-main.5056b2ee.js"
+  },
+  {
+    "revision": "3983ac194e86cc2efad1701c62ead539",
+    "url": "/react_flip_card/static/media/dance.3983ac19.jpg"
+  },
+  {
+    "revision": "7bd3de8652ab9b3f6cac9d7add69ae55",
+    "url": "/react_flip_card/static/media/travel.7bd3de86.jpg"
+  }
+]);
